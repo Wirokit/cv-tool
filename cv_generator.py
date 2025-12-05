2142976eddd0553291599a9b448a94b82036bdf8
Sami Pitkänen <sami.pitkanen@wirokit.com>
2025-12-05 11:13:22 +0200

    Web tab icon

--- cv_generator.py
+++ cv_generator.py
@@ -1,6 +1,5 @@
 # Initial file by Gemini
 import json
-import os
 
 
 def generate_professional_cv(
@@ -207,6 +206,7 @@ def generate_professional_cv(
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="/static/images/favicon.ico" />
         <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
